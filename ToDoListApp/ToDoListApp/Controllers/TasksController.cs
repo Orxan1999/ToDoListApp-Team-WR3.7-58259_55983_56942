7bac8b6f4684f4cf6385bdfdb76866bfb74e9475
Orxan1999 <orxanm32@gmail.com>
2024-06-10 10:57:29 +0200

    Implemeting View

---
+++ ToDoListApp/ToDoListApp/Controllers/TasksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
