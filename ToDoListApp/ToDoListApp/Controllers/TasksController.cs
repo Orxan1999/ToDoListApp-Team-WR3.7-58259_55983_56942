b8db24f1a76c66f92cb4acea5013a66f31fbd1db
KamalAzizov <azizovkamal54@gmail.com>
2024-06-10 15:11:27 +0200

    Implementing Create Action

--- ToDoListApp/ToDoListApp/Controllers/TasksController.cs
+++ ToDoListApp/ToDoListApp/Controllers/TasksController.cs
@@ -19,5 +19,40 @@ namespace ToDoListApp.Controllers
             ViewBag.Count = 1;
             return View(tasks);
         }
+
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(TaskModel task)
+        {
+            task.Title = task.Title?.Trim();
+            bool isExists = await _db.Tasks.AnyAsync(x => x.Title == task.Title);
+
+            if (isExists)
+            {
+                ModelState.AddModelError("Title", "There is already task under this title. Please change the title");
+                return View(task);
+            }
+
+            if (task.Deadline < DateTime.Now)
+            {
+                ModelState.AddModelError("Deadline", "Deadline can not be earlier than current time");
+                return View(task);
+
+            }
+
+            await _db.Tasks.AddAsync(task);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+
+        }
+
     }
 }
